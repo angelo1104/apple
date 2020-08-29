3170486bc86f83ab19b7c07c8a4785e3d7333139
Angelo <angelo@american.com>
2020-08-29 10:45:28 +0530

    Only tv+ is left

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,7 @@
 if (window.outerWidth>= 450){
     $("#small-iphone11-img").addClass("hide");
+
+    //$('#apple-tv-col').attr("style","background: url('assets/img/new-apple-tv-medium.jpg') center / contain no-repeat");
+
+    document.querySelector("#apple-tv-col").setAttribute("style","background: url('assets/img/new-apple-tv-small.jpg') center / contain no-repeat");
 }
