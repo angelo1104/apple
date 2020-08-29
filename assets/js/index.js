39e0e781c65e4ed9f7afd9add29f98d3ec766574
Angelo <angelo@american.com>
2020-08-29 14:42:09 +0530

    Done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,46 @@
 if (window.outerWidth>= 450){
     $("#small-iphone11-img").addClass("hide");
-
-    //$('#apple-tv-col').attr("style","background: url('assets/img/new-apple-tv-medium.jpg') center / contain no-repeat");
-
-    document.querySelector("#apple-tv-col").setAttribute("style","background: url('assets/img/new-apple-tv-small.jpg') center / contain no-repeat");
 }
+
+$("#products-iphone-se").click(function(){
+    window.location.href = "https://www.apple.com/iphone-se/"
+})
+
+$("#products-iphone-pro").click(function(){
+    window.location.href = "https://www.apple.com/iphone-11/"
+})
+
+$("#products-ipad-pro").click(function(){
+    window.location.href = "https://www.apple.com/ipad-pro/"
+})
+
+$("#small-product-mac").click(function(){
+    window.location.href = "https://www.apple.com/us-hed/shop/back-to-school"
+})
+
+$("#small-product-airpods").click(function(){
+    window.location.href = "https://www.apple.com/airpods/"
+})
+
+$("#apple-card-payment").click(function(){
+    window.location.href = "https://www.apple.com/apple-card/monthly-installments"
+})
+
+$("#apple-tv-col").click(function(){
+    window.location.href = "https://tv.apple.com/show/little-voice/umc.cmc.4x932osv992wjjdq7ce12za2r?itscg=10000&itsct=atv-0-apl_hp-pmo_wch-lvc-200710"
+})
+
+$("#small-product-watch").click(function(){
+    window.location.href = "https://www.apple.com/apple-watch-series-5/"
+})
+
+$("#apple-letter").click(function(){
+    window.location.href = "https://www.apple.com/store-opening-letter/"
+})
+
+window.onload= ()=>{
+    document.querySelector("#apple-pay-parks").addEventListener("click",function(){
+        window.location.href = "https://www.apple.com/apple-pay/"
+    })
+}
+
